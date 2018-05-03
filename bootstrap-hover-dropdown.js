8b2c8e00e675bafcb92f48bf6e165ab3b11cf48d
Simon Eames <simon@bcaresearch.com>
2018-05-03 11:59:09 +1000

    remove depreciated jquery method hover
    The method is shorthand for and replaced by calls to mouseenter and mouse leave

--- bootstrap-hover-dropdown.js
+++ bootstrap-hover-dropdown.js
@@ -2,7 +2,7 @@
  * @preserve
  * Project: Bootstrap Hover Dropdown
  * Author: Cameron Spear
- * Version: v2.2.1
+ * Version: v2.2.2
  * Contributors: Mattia Larentis
  * Dependencies: Bootstrap's Dropdown plugin, jQuery
  * Description: A simple plugin to enable Bootstrap dropdowns to active on hover and provide a nice user experience.
@@ -45,27 +45,30 @@
                 settings = $.extend(true, {}, defaults, options, data),
                 timeout, timeoutHover;
 
-            $parent.hover(function (event) {
-                // so a neighbor can't open the dropdown
-                if(!$parent.hasClass('open') && !$this.is(event.target)) {
-                    // stop this event, stop executing any code
-                    // in this callback but continue to propagate
-                    return true;
-                }
+            $parent.on({
+                'mouseenter': function (event) {
+                    // so a neighbor can't open the dropdown
+                    if(!$parent.hasClass('open') && !$this.is(event.target)) {
+                        // stop this event, stop executing any code
+                        // in this callback but continue to propagate
+                        return true;
+                    }
 
-                openDropdown(event);
-            }, function () {
-                // clear timer for hover event
-                window.clearTimeout(timeoutHover)
-                timeout = window.setTimeout(function () {
-                    $this.attr('aria-expanded', 'false');
-                    $parent.removeClass('open');
-                    $this.trigger(hideEvent);
-                }, settings.delay);
+                    openDropdown(event);
+                },
+                'mouseleave': function () {
+                    // clear timer for hover event
+                    window.clearTimeout(timeoutHover)
+                    timeout = window.setTimeout(function () {
+                        $this.attr('aria-expanded', 'false');
+                        $parent.removeClass('open');
+                        $this.trigger(hideEvent);
+                    }, settings.delay);
+                }
             });
 
             // this helps with button groups!
-            $this.hover(function (event) {
+            $this.on('mouseenter mouseleave', function (event) {
                 // this helps prevent a double event from firing.
                 // see https://github.com/CWSpear/bootstrap-hover-dropdown/issues/55
                 if(!$parent.hasClass('open') && !$parent.is(event.target)) {
@@ -81,16 +84,19 @@
             $parent.find('.dropdown-submenu').each(function (){
                 var $this = $(this);
                 var subTimeout;
-                $this.hover(function () {
-                    window.clearTimeout(subTimeout);
-                    $this.children('.dropdown-menu').show();
-                    // always close submenu siblings instantly
-                    $this.siblings().children('.dropdown-menu').hide();
-                }, function () {
-                    var $submenu = $this.children('.dropdown-menu');
-                    subTimeout = window.setTimeout(function () {
-                        $submenu.hide();
-                    }, settings.delay);
+                $this.on({
+                    'mouseenter':function () {
+                        window.clearTimeout(subTimeout);
+                        $this.children('.dropdown-menu').show();
+                        // always close submenu siblings instantly
+                        $this.siblings().children('.dropdown-menu').hide();
+                    },
+                    'mouseleave': function () {
+                        var $submenu = $this.children('.dropdown-menu');
+                        subTimeout = window.setTimeout(function () {
+                            $submenu.hide();
+                        }, settings.delay);
+                    }
                 });
             });
 
@@ -105,14 +111,14 @@
                 window.clearTimeout(timeout);
                 // restart hover timer
                 window.clearTimeout(timeoutHover);
-                
-                // delay for hover event.  
+
+                // delay for hover event.
                 timeoutHover = window.setTimeout(function () {
                     $allDropdowns.find(':focus').blur();
 
                     if(settings.instantlyCloseOthers === true)
                         $allDropdowns.removeClass('open');
-                    
+
                     // clear timer for hover event
                     window.clearTimeout(timeoutHover);
                     $this.attr('aria-expanded', 'true');
@@ -123,7 +129,7 @@
         });
     };
 
-    $(document).ready(function () {
+    $(function () {
         // apply dropdownHover to all elements with the data-hover="dropdown" attribute
         $('[data-hover="dropdown"]').dropdownHover();
     });
